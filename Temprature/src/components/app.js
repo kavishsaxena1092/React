2cf61de9e459e4d73087bd933f05cb7539fecc5e
kavishsaxena1092 <kavishaxena1092@gmail.com>
2018-08-31 11:27:45 +0530

    Temperature Project

---
+++ Temprature/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Searchbar from '../container/search_bar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Searchbar />
+      </div>
+    );
+  }
+}
